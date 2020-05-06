ebacd00bfbf7aea0013985f5e07415449b4392b1
belen <belenvargaslopez@gmail.com>
2020-05-06 18:27:19 -0400

    third commit portafolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  var TopOffset = 85;    
+
+$('a[data-smooth]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - TopOffset
+          }, 1000);
+          return false;
+        }
+      }
+    });
